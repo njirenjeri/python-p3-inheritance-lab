91c468c4479c942cb4e048de83fd54df05f60995
Njire Njeri <njirenjeri@gmail.com>
2025-03-03 14:29:40 +0300

    inhertance lab complete

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
+    def learn(self, knowledge_input):
+        if isinstance (knowledge_input, str):
+            return self.knowledge.append(knowledge_input)
         pass
